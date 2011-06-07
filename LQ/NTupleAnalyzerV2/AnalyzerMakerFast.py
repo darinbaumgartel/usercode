25388d338529b9e88ca7ea3dc1842463d55208eb
darinb <>
2011-06-07 09:33:05 +0000

    Added example analyzer directory

--- LQ/NTupleAnalyzerV2/AnalyzerMakerFast.py
+++ LQ/NTupleAnalyzerV2/AnalyzerMakerFast.py
@@ -8,11 +8,26 @@ import os # For direcory scanning abilities, etc
 from time import strftime
 import sys
 
-myfile = sys.argv[1]
+cfile = ''
+hfile = ''
+ifile = ''
+
+a = sys.argv
+for x in range(len(a)):
+	if a[x] == '-i':
+		ifile = a[x+1]
+	if a[x] == '-c':
+		cfile = a[x+1]
+	if a[x] == '-h':
+		hfile = a[x+1]	
+
+if cfile == '' or hfile == '' or ifile == '':
+	print 'Must specify input .C, .h, and .csv files, e.g.:\n\npython AnalyzerMakerFast.py -i NTupleInfoSpring2011.csv -c NTupleAnalyzer.C -h NTupleAnalyzer.h\n\n   Exiting   \n\n'
+	sys.exit()
 
 ### This portion reads the NTupleInfo.csv file and gets the information ###
 import csv
-csvfile = open(myfile,'r')
+csvfile = open(ifile,'r')
 table=[]
 
 for row in csv.reader(csvfile):
@@ -33,7 +48,7 @@ for x in range(0,len(HLTBit)):
 ###----------------------------------------------------------------------###
 
 ### This portion creates the branch status commands to speed up the code ###
-f1 = open('NTupleAnalyzer.h','r')
+f1 = open(hfile,'r')
 statuscommands = ''
 
 variables = []
@@ -43,7 +58,7 @@ for line in f1:
 		variables.append( ((var.replace('\n','')).replace('\r','')).replace(' ','')  )
 for x in variables:
 	use =0
-	f2 = open('NTupleAnalyzer.C','r')
+	f2 = open(cfile,'r')
 	for line in f2:
 		if x in line:	
 			use = 1
@@ -53,6 +68,9 @@ for x in variables:
 		statuscommands += '   fChain->SetBranchStatus("'+x+'",0);\n'
 	f2.close()
 f1.close()
+
+c2file = cfile.split('/')[-1]
+
 ###----------------------------------------------------------------------###
 
 now=str(strftime("%Y-%m-%d-%H:%M:%S"))
@@ -60,10 +78,7 @@ now = now.replace(" ","")
 now = now.replace("\t","")
 now = now.replace("-","_")
 now = now.replace(":","_")
-#os.system("mkdir ../Stage1_NTupleAnalysis/ROOTBAK_"+now)
-#os.system("mv ../Stage1_NTupleAnalysis/CurrentRootFiles/*.root ../Stage1_NTupleAnalysis/ROOTBAK_"+now)
-#os.system("rm ../Stage1_NTupleAnalysis/*_*.* ../Stage1_NTupleAnalysis/RootProcesses")
-# Analysis Details Below
+
 WhatType=['Background','Signal','CollisionData'] # So that WhatType[SigOrBG[x]] returns string "Signal" or "Background"
 
 # initialize file that stores root processes to be run
@@ -74,8 +89,8 @@ f_sub.write('#!/bin/csh')
 
 thisdir = os.popen('pwd').readlines()[0].replace('\n','')
 person = os.popen('whoami').readlines()[0].replace('\n','')
-thiscastor = '/castor/cern.ch/user/'+person[0]+'/'+person+'/LQAnalyzerOutput/NTupleAnalyzer_'+now
-os.system('rfmkdir '+ thiscastor.replace('NTupleAnalyzer_'+now,''))
+thiscastor = '/castor/cern.ch/user/'+person[0]+'/'+person+'/LQAnalyzerOutput/'+c2file.replace('.C','')+'_'+now
+os.system('rfmkdir '+ thiscastor.replace(c2file.replace('.C','')+'_'+now,''))
 os.system('rfmkdir '+ thiscastor)
 
 bjobs = []
@@ -108,7 +123,7 @@ for x in range(len(SignalType)):
 		part = 'part'+str(y+1)+'of'+str(len(newdirList))
 
 		sub_thisroot = open("sub_"+SignalType[x]+part+".csh",'w')
-		sub_thisroot.write('#!/bin/csh\ncd '+thisdir+'\neval `scramv1 runtime -csh`\ncd -\ncp '+thisdir+'/NTupleAnalyzer_'+SignalType[x].replace('-','_')+part+'.* .\ncp '+thisdir+'/JSONFilterFunction.* .\ncp '+thisdir+'/RootProcesses_'+SignalType[x]+part+' .\nroot -b RootProcesses_'+SignalType[x]+part+'\nrfcp '+SignalType[x].replace('-','_')+part+'.root '+thiscastor+'\n')
+		sub_thisroot.write('#!/bin/csh\ncd '+thisdir+'\neval `scramv1 runtime -csh`\ncd -\ncp '+thisdir+'/'+c2file.replace('.C','')+'_'+SignalType[x].replace('-','_')+part+'.* .\ncp '+thisdir+'/JSONFilterFunction.* .\ncp '+thisdir+'/RootProcesses_'+SignalType[x]+part+' .\nroot -b RootProcesses_'+SignalType[x]+part+'\nrfcp '+SignalType[x].replace('-','_')+part+'.root '+thiscastor+'\n')
 		sub_thisroot.close()
 
 		f_sub.write('\nsleep 1\nbsub -R "pool>10000" -o /dev/null -e /dev/null -q 1nd -J job'+SignalType[x]+part+' < sub_'+SignalType[x]+part+'.csh\n')
@@ -117,13 +132,13 @@ for x in range(len(SignalType)):
 
 
 		f_thisroot.write('{\n')
-		RootLinesToAdd = 'gROOT->ProcessLine(\"gErrorIgnoreLevel = 3001;\");\ngROOT->ProcessLine(\".L NTupleAnalyzer_'+ SignalType[x].replace('-','_') + part+'.C++\");\ngROOT->ProcessLine(\"NTupleAnalyzer_'+ SignalType[x].replace('-','_') + part+' t\");\ngROOT->ProcessLine(\"t.Loop()\");\ngROOT->ProcessLine(\"gROOT->Reset()\");\ngROOT->ProcessLine(".q");\n}\n'
+		RootLinesToAdd = 'gROOT->ProcessLine(\"gErrorIgnoreLevel = 3001;\");\ngROOT->ProcessLine(\".L '+c2file.replace('.C','')+'_'+ SignalType[x].replace('-','_') + part+'.C++\");\ngROOT->ProcessLine(\"'+c2file.replace('.C','')+'_'+ SignalType[x].replace('-','_') + part+' t\");\ngROOT->ProcessLine(\"t.Loop()\");\ngROOT->ProcessLine(\"gROOT->Reset()\");\ngROOT->ProcessLine(".q");\n}\n'
 		f_thisroot.write(RootLinesToAdd)
 		f_thisroot.close()
 
-		bjobs.append('NTupleAnalyzer_'+ SignalType[x].replace('-','_') + part )
+		bjobs.append(''+c2file.replace('.C','')+'_'+ SignalType[x].replace('-','_') + part )
 
-		s1 = open("NTupleAnalyzer.C").read() # Open the NTupleAnalyzer.C template and replace with array values
+		s1 = open(cfile).read() # Open the NTupleAnalyzer.C template and replace with array values
 		s1 = s1.replace('Numberofevents',str(float(N_orig[x])))
 		s1 = s1.replace('placeholder', SignalType[x].replace('-','_')+part)
 		s1 = s1.replace('crosssection', str(float(Xsections[x])))
@@ -138,13 +153,13 @@ for x in range(len(SignalType)):
 			s1 = s1.replace('IsItWMC', 'false')
 	
 	#	os.system('rm NTupleAnalyzer_'+SignalType[x]+'.*')
-		f1 = open("NTupleAnalyzer_"+SignalType[x].replace('-','_')+part+".C", 'w') # write a new file based on the template
+		f1 = open(c2file.replace('.C','')+'_'+SignalType[x].replace('-','_')+part+".C", 'w') # write a new file based on the template
 		f1.write(s1)
 		f1.close()
 		
 	
-		s2 = open("NTupleAnalyzer.h") # Same deal for the .h file now...
-		f2 = open("NTupleAnalyzer_"+SignalType[x].replace('-','_')+part+".h", 'w')
+		s2 = open(hfile) # Same deal for the .h file now...
+		f2 = open(c2file.replace('.C','')+'_'+SignalType[x].replace('-','_')+part+".h", 'w')
 		for line in s2.readlines():
 			line = line.replace('placeholder', SignalType[x].replace('-','_')+part)
 			f2.write(line)
@@ -164,13 +179,13 @@ for x in range(len(SignalType)):
 	
 	# The next task is to write the root commands to a file for running using the gROOT->ProcessLine commands
 	# Note here that "gErrorIgnoreLevel = 3001" stops the issuance of warnings and errors, as a number of known-irrelevant  errors can appear. Use only if you are sure of your code :)
-	RootLinesToAdd = 'gROOT->ProcessLine(\"gErrorIgnoreLevel = 3001;\");\ngROOT->ProcessLine(\".L NTupleAnalyzer_'+ SignalType[x] + '.C++\");\ngROOT->ProcessLine(\"NTupleAnalyzer_'+ SignalType[x] + ' t\");\ngROOT->ProcessLine(\"t.Loop()\");\ngROOT->ProcessLine(\"gROOT->Reset()\");\n\n'
-	
-	# Below I give an alternate version with errors not supressed (no GErrorIgnoreLevel). Choose wisely.
-	# # # RootLinesToAdd = 'gROOT->ProcessLine(\".L NTupleAnalyzer_'+ SignalType[x] + '.C++\");\ngROOT->ProcessLine(\"NTupleAnalyzer_'+ SignalType[x] + ' t\");\ngROOT->ProcessLine(\"t.Loop()\");\ngROOT->ProcessLine(\"gROOT->Reset()\");\n\n'
+	RootLinesToAdd = 'gROOT->ProcessLine(\"gErrorIgnoreLevel = 3001;\");\ngROOT->ProcessLine(\".L '+c2file.replace('.C','')+'_'+ SignalType[x] + '.C++\");\ngROOT->ProcessLine(\"'+c2file.replace('.C','')+'_'+ SignalType[x] + ' t\");\ngROOT->ProcessLine(\"t.Loop()\");\ngROOT->ProcessLine(\"gROOT->Reset()\");\n\n'
 	
 f_sub.close()
 os.system('chmod 777 sub*.csh')
+
+sys.exit()
+
 
 os.system('./sub_AllAnalyzer.csh')
 
@@ -192,7 +207,7 @@ castorinfo = os.popen('nsls '+thiscastor).readlines()
 castorinfo = str(castorinfo)
 ok = 1
 for x in bjobs:
-	if x.replace('NTupleAnalyzer_','') not in castorinfo:
+	if x.replace(c2file.replace('.C','')+'_','') not in castorinfo:
 		ok = 0
 		print (x +' is missing from castor directory.')	
 
@@ -202,7 +217,7 @@ if ok == 1:
 castorinfo = os.popen('nsls '+thiscastor).readlines()
 
 os.system
-thistemp = '/tmp/'+person+'/NTupleAnalyzer_'+now
+thistemp = '/tmp/'+person+'/'+c2file.replace('.C','')+'_'+now
 os.system('mkdir '+thistemp)
 
 print ('Waiting for file transfers to complete\n\n')
@@ -219,7 +234,7 @@ while ok!=1:
 	tempinfo = str(tempinfo)
 	os.system('sleep 20')
 	for x in bjobs:
-		if x.replace('NTupleAnalyzer_','') not in tempinfo:
+		if x.replace(c2file.replace('.C','')+'_','') not in tempinfo:
 			unfin += 1
 			ok = 0
 	print 'Waiting on '+str(unfin)+' files to transfer.\n' 
