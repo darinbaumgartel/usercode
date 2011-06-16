c3075689ed6439577095470d10a0bc70e50674d9
darinb <>
2011-06-16 08:33:06 +0000

    Switch default to madgraph

--- LQ/NTupleAnalyzerV2/PlotMacros/LoadCastorFiles.C
+++ LQ/NTupleAnalyzerV2/PlotMacros/LoadCastorFiles.C
@@ -5,7 +5,7 @@ TFile *f__CurrentData = TFile::Open = TFile::Open("rfio:/castor/cern.ch/user/d/d
 //TFile f__lqmumu(" rfio:/castor/cern.ch/user/d/darinb/LQAnalyzerOutput/NTupleAnalyzer_V00_02_04_VBTFTight_2011_06_10_12_28_30/SummaryFiles/LQToCMu_M_400.root");
 TFile *f__WJets = TFile::Open("rfio:/castor/cern.ch/user/d/darinb/LQAnalyzerOutput/NTupleAnalyzer_V00_02_04_VBTFTight_2011_06_10_12_28_30/SummaryFiles/WJetsMG.root");
 TFile *f__TTbarJets = TFile::Open("rfio:/castor/cern.ch/user/d/darinb/LQAnalyzerOutput/NTupleAnalyzer_V00_02_04_VBTFTight_2011_06_10_12_28_30/SummaryFiles/TTBar.root");
-TFile *f__ZJets = TFile::Open("rfio:/castor/cern.ch/user/d/darinb/LQAnalyzerOutput/NTupleAnalyzer_V00_02_04_VBTFTight_2011_06_10_12_28_30/SummaryFiles/ZJetsALP.root");
+TFile *f__ZJets = TFile::Open("rfio:/castor/cern.ch/user/d/darinb/LQAnalyzerOutput/NTupleAnalyzer_V00_02_04_VBTFTight_2011_06_10_12_28_30/SummaryFiles/ZJetsMG.root");
 TFile *f__VVJets = TFile::Open("rfio:/castor/cern.ch/user/d/darinb/LQAnalyzerOutput/NTupleAnalyzer_V00_02_04_VBTFTight_2011_06_10_12_28_30/SummaryFiles/DiBoson.root");
 TFile *f__SingleTop = TFile::Open("rfio:/castor/cern.ch/user/d/darinb/LQAnalyzerOutput/NTupleAnalyzer_V00_02_04_VBTFTight_2011_06_10_12_28_30/SummaryFiles/SingleTop.root");
 TFile *f__QCDMu = TFile::Open("rfio:/castor/cern.ch/user/d/darinb/LQAnalyzerOutput/NTupleAnalyzer_V00_02_04_VBTFTight_2011_06_10_12_28_30/SummaryFiles/QCDMu.root");
