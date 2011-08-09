3ca2f70ad9497cf7c23f2e4fe0b58df897885fec
darinb <>
2011-08-09 17:28:00 +0000

    QCD non-iso study files

---
+++ LQ/NTupleAnalyzerV2/PlotMacros/LoadCastorFilesQCDStudy.C
@@ -0,0 +1,69 @@
+{
+
+TFile *f__CurrentData = TFile::Open = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/SingleMuData.root");
+//TFile f__lq("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_MuNuJJFilter_M_300.root");
+
+TFile *f__lqmumu250 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_M_250.root");
+TFile *f__lqmumu350 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_M_350.root");
+TFile *f__lqmumu400 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_M_400.root");
+TFile *f__lqmumu450 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_M_450.root");
+TFile *f__lqmumu500 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_M_500.root");
+TFile *f__lqmumu550 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_M_550.root");
+TFile *f__lqmumu600 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_M_600.root");
+TFile *f__lqmumu650 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_M_650.root");
+TFile *f__lqmumu750 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_M_750.root");
+TFile *f__lqmumu850 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_M_850.root");
+
+TFile *f__lqmunu250 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_BetaHalf_M_250.root");
+TFile *f__lqmunu350 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_BetaHalf_M_350.root");
+TFile *f__lqmunu400 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_BetaHalf_M_400.root");
+TFile *f__lqmunu450 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_BetaHalf_M_450.root");
+TFile *f__lqmunu500 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_BetaHalf_M_500.root");
+TFile *f__lqmunu550 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_BetaHalf_M_550.root");
+TFile *f__lqmunu600 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_BetaHalf_M_600.root");
+TFile *f__lqmunu650 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_BetaHalf_M_650.root");
+TFile *f__lqmunu750 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_BetaHalf_M_750.root");
+TFile *f__lqmunu850 = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/LQToCMu_BetaHalf_M_850.root");
+
+
+TFile *f__WJets = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/WJets.root");
+TFile *f__TTbarJets = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/TTBar.root");
+TFile *f__ZJets = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/ZJetsMG.root");
+TFile *f__VVJets = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/DiBoson.root");
+TFile *f__SingleTop = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/SingleTop.root");
+TFile *f__QCDMu = TFile::Open("~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_QCDStudy_StandardSelections_ForLP_QCDStudy_2011_08_09_04_48_31/SummaryFiles/QCDMu.root");
+
+
+TTree* wjets = (TTree* )f__WJets->Get("PhysicalVariables");
+TTree* ttbar = (TTree* )f__TTbarJets->Get("PhysicalVariables");
+TTree* zjets = (TTree* )f__ZJets->Get("PhysicalVariables");
+TTree* vvjets = (TTree* )f__VVJets->Get("PhysicalVariables");
+TTree* singtop = (TTree* )f__SingleTop->Get("PhysicalVariables");
+TTree* qcd = (TTree* )f__QCDMu->Get("PhysicalVariables");
+TTree* data = (TTree* )f__CurrentData->Get("PhysicalVariables");
+
+//TTree* lq = (TTree* )f__lq->Get("PhysicalVariables");
+
+TTree* lqmumu250 = (TTree* )f__lqmumu250->Get("PhysicalVariables");
+TTree* lqmumu350 = (TTree* )f__lqmumu350->Get("PhysicalVariables");
+TTree* lqmumu400 = (TTree* )f__lqmumu400->Get("PhysicalVariables");
+TTree* lqmumu450 = (TTree* )f__lqmumu450->Get("PhysicalVariables");
+TTree* lqmumu500 = (TTree* )f__lqmumu500->Get("PhysicalVariables");
+TTree* lqmumu550 = (TTree* )f__lqmumu550->Get("PhysicalVariables");
+TTree* lqmumu600 = (TTree* )f__lqmumu600->Get("PhysicalVariables");
+TTree* lqmumu650 = (TTree* )f__lqmumu650->Get("PhysicalVariables");
+TTree* lqmumu750 = (TTree* )f__lqmumu750->Get("PhysicalVariables");
+TTree* lqmumu850 = (TTree* )f__lqmumu850->Get("PhysicalVariables");
+
+TTree* lqmunu250 = (TTree* )f__lqmunu250->Get("PhysicalVariables");
+TTree* lqmunu350 = (TTree* )f__lqmunu350->Get("PhysicalVariables");
+TTree* lqmunu400 = (TTree* )f__lqmunu400->Get("PhysicalVariables");
+TTree* lqmunu450 = (TTree* )f__lqmunu450->Get("PhysicalVariables");
+TTree* lqmunu500 = (TTree* )f__lqmunu500->Get("PhysicalVariables");
+TTree* lqmunu550 = (TTree* )f__lqmunu550->Get("PhysicalVariables");
+TTree* lqmunu600 = (TTree* )f__lqmunu600->Get("PhysicalVariables");
+TTree* lqmunu650 = (TTree* )f__lqmunu650->Get("PhysicalVariables");
+TTree* lqmunu750 = (TTree* )f__lqmunu750->Get("PhysicalVariables");
+TTree* lqmunu850 = (TTree* )f__lqmunu850->Get("PhysicalVariables");
+
+}
