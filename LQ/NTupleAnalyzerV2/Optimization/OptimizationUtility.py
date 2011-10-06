af6995920bee838eebf05db95fd78646c966f0c3
darinb <>
2011-10-06 21:43:02 +0000

    changed for several optimization methods

--- LQ/NTupleAnalyzerV2/Optimization/OptimizationUtility.py
+++ LQ/NTupleAnalyzerV2/Optimization/OptimizationUtility.py
@@ -10,7 +10,7 @@ import math
 AnalysisType=sys.argv[1] 
 #FileLocation="~/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_04_Default__2011_07_12_16_27_56/SummaryFiles/"
 #FileLocation="/tmp/darinb/NTupleAnalyzer_V00_02_04_Default__2011_07_12_16_27_56/SummaryFiles/"
-FileLocation="/afs/cern.ch/user/d/darinb/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_StandardSelections_2011_07_21_16_14_42/SummaryFiles/"
+FileLocation="/afs/cern.ch/user/d/darinb/neuhome/LQAnalyzerOutput/NTupleAnalyzer_V00_02_05_Default_StandardSelections_2fb_2011_10_04_15_37_02/SummaryFiles/"
 
 #print(AnalysisType)
 if AnalysisType == "":
@@ -18,14 +18,18 @@ if AnalysisType == "":
 	sys.exit(0)
 from time import strftime
 
-ttbarscale = 165./121.
-wscale = 1.19
-zscale = 1.34
+ttbarscale = 1.01
+wscale = 1.24
+zscale = 8.723
+
+ErrorFactorMuMu = 0.25
+ErrorFactorMuNu = 0.9
+
 CutVariablesMuMu=['ST_pf_mumu','M_muon1muon2','LowestMass_BestLQCombo'] # Which variables do you want to cut on
 
-VariableStartingPointMuMu = [250,100,50] # Where to start cutting on the variable 
+VariableStartingPointMuMu = [250,100,100] # Where to start cutting on the variable 
 VariableIntervalMuMu = [10,10,10] # Intervals in which you will test cuts
-VariablePointsToTestMuMu = [85,12,80] # Number of cutting points to test at the given interval
+VariablePointsToTestMuMu = [85,12,90] # Number of cutting points to test at the given interval
 
 
 #CutVariablesMuNu=['ST_pf_munu','MT_muon1pfMET','M_bestmupfjet_munu'] # Which variables do you want to cut on
@@ -35,21 +39,20 @@ CutVariablesMuNu=['ST_pf_munu','MET_pf','M_bestmupfjet_munu'] # Which variables 
 #VariableIntervalMuNu = [10,5,10] # Intervals in which you will test cuts
 #VariablePointsToTestMuNu = [85,30,80] # Number of cutting points to test at the given interval
 
-VariableStartingPointMuNu = [730,140,300] # Where to start cutting on the variable 
-VariableIntervalMuNu = [10,5,10] # Intervals in which you will test cuts
-VariablePointsToTestMuNu = [30,30,40] # Number of cutting points to test at the given interval
+VariableStartingPointMuNu = [250,100,100] # Where to start cutting on the variable 
+VariableIntervalMuNu = [10,10,10] # Intervals in which you will test cuts
+VariablePointsToTestMuNu = [85,20,90] # Number of cutting points to test at the given interval
 person = (os.popen("whoami").readlines())[0].replace('\n','')
 
 # Other variable to precut on as they appear in the root file (these are not variable cuts, they are single static cuts):
-lumi= 964.0
-
+lumi= 2000.0
 preselectionmumu = '((Pt_muon1>40)*(Pt_muon2>40)*(Pt_pfjet1>30)*(Pt_pfjet2>30)*(ST_pf_mumu>250)*(deltaR_muon1muon2>0.3)*(M_muon1muon2>100)*(LowestMass_BestLQCombo>50)*((abs(Eta_muon1)<2.1)||(abs(Eta_muon2)<2.1)))'
-preselectionmunu = '((Pt_muon1>40)*(Pt_muon2<30.0)*(MET_pf>45)*(Pt_pfjet1>30)*(Pt_pfjet2>30)*(Pt_ele1<15.0)*(ST_pf_munu>250)*(abs(Eta_muon1)<2.1))*(abs(deltaPhi_muon1pfMET)>.8)*(abs(deltaPhi_pfjet1pfMET)>.5)*(FailIDPFThreshold<25.0)*(MT_muon1pfMET>50.0)*(M_bestmupfjet_munu>50)'
+preselectionmunu = '((Pt_muon1>40)*(Pt_muon2<15.0)*(MET_pf>45)*(Pt_pfjet1>30)*(Pt_pfjet2>30)*(Pt_ele1<15.0)*(ST_pf_munu>250)*(abs(Eta_muon1)<2.1))*(abs(deltaPhi_muon1pfMET)>.8)*(abs(deltaPhi_pfjet1pfMET)>.5)*(FailIDPFThreshold<25.0)*(MT_muon1pfMET>50.0)*(M_bestmupfjet_munu>50)'
 
 for x in range(len(CutVariablesMuNu)):
 	preselectionmunu += '*('+CutVariablesMuNu[x]+'> '+str(VariableStartingPointMuNu[x])+')'
 
-cut_mc = str(lumi)+'*weight_964pileup_bugfix'
+cut_mc = str(lumi)+'*weight_pileup2fb'
 # These are MC - driven Summer11 for the bug fix
 #cut_mc += "*(";
 #cut_mc += "((N_PileUpInteractions > -0.5)*(N_PileUpInteractions < 0.5)*(0.133))+";
@@ -84,10 +87,41 @@ cut_mc = str(lumi)+'*weight_964pileup_bugfix'
 #cut_mc += "((N_PileUpInteractions > 28.5)*(N_PileUpInteractions < 29.5)*(3.35058351289e-05))";
 #cut_mc += ")";
 	
+KeepFiles = ['LQToCMu_BetaHalf_M_250.root',
+'LQToCMu_BetaHalf_M_350.root',
+'LQToCMu_BetaHalf_M_400.root',
+'LQToCMu_BetaHalf_M_450.root',
+'LQToCMu_BetaHalf_M_500.root',
+'LQToCMu_BetaHalf_M_550.root',
+'LQToCMu_BetaHalf_M_600.root',
+'LQToCMu_BetaHalf_M_650.root',
+'LQToCMu_BetaHalf_M_750.root',
+'LQToCMu_BetaHalf_M_850.root',
+'LQToCMu_M_250.root',
+'LQToCMu_M_350.root',
+'LQToCMu_M_400.root',
+'LQToCMu_M_450.root',
+'LQToCMu_M_500.root',
+'LQToCMu_M_550.root',
+'LQToCMu_M_600.root',
+'LQToCMu_M_650.root',
+'LQToCMu_M_750.root',
+'LQToCMu_M_850.root',
+'DiBoson.root',
+'SingleTop.root',
+'TTBar.root',
+'WJets_Sherpa.root',
+'ZJets_Sherpa.root']	
+	
 preselectionmumu = preselectionmumu
 preselectionmunu = preselectionmunu
 
 logout = open(AnalysisType+"_log.txt",'w')
+logoutSB = open(AnalysisType+"_SB_log.txt",'w')
+logoutSSB = open(AnalysisType+"_SSB_log.txt",'w')
+logoutSSBdB2 = open(AnalysisType+"_SSBdB2_log.txt",'w')
+
+BGErrorFactor = 0.0
 
 if AnalysisType=="MuMu" or AnalysisType =="MuMu" or AnalysisType=="mumu":
 	CutVariables = CutVariablesMuMu
@@ -95,12 +129,15 @@ if AnalysisType=="MuMu" or AnalysisType =="MuMu" or AnalysisType=="mumu":
 	VariableInterval = VariableIntervalMuMu 
 	VariablePointsToTest= VariablePointsToTestMuMu
 	preselection = preselectionmumu
+	BGErrorFactor =  ErrorFactorMuMu	
 if AnalysisType=="MuNu" or AnalysisType =="Munu" or AnalysisType=="munu":
 	CutVariables = CutVariablesMuNu
 	VariableStartingPoint = VariableStartingPointMuNu 
 	VariableInterval = VariableIntervalMuNu 
 	VariablePointsToTest= VariablePointsToTestMuNu	
 	preselection = preselectionmunu
+	BGErrorFactor =  ErrorFactorMuNu	
+
 if AnalysisType!="MuNu" and AnalysisType !="Munu" and AnalysisType!="munu" and AnalysisType!="MuMu" and AnalysisType !="MuMu" and AnalysisType!="mumu":
 	print("\n\nAnalysis Type must be specified as MuMu or MuNu\n\n Example: \n >> python OptimizationUtility.py MuNu\n\nProcess Exiting")
 	sys.exit(0)
@@ -121,6 +158,12 @@ SignalType = []
 SigOrBG = []
 for x in fileinfo:
 	dirfiles.append( x.replace('\n',''))
+	tocontinue = 1
+	for k in KeepFiles:
+		if k in x:
+			tocontinue = 0
+	if (tocontinue):
+		continue
 	SignalType.append(x.replace('.root','').replace('\n',''))
 	if 'LQ' in x:
 		SigOrBG.append(1)
@@ -157,6 +200,7 @@ if AnalysisType=="MuMu" or AnalysisType =="MuMu" or AnalysisType=="mumu":
 	SigOrBG=SBList
 	SignalType=LQList
 
+print LQList
 
 def multi_for(iterables):
 	if not iterables:
@@ -169,6 +213,11 @@ def multi_for(iterables):
 BackgroundValues = []
 SignalValues = []
 SignifValues = []
+
+SignifValuesSSB = []
+SignifValuesSB = []
+SignifValuesSSBdB2 = []
+
 cuts = []
 n = 0
 for i in multi_for(map(xrange, VariablePointsToTest)):
@@ -210,12 +259,11 @@ for x in range(len(SignalType)):
 	h = TH1F('h','h',1,0,2)
 	print "Evaluating Cuts... " 
 	for y in range(len(cuts)):
-		##print cut_mc+'*'+str(scalefactor)+"*"+cuts[y]
 		T.Project('h','1.0',cut_mc+'*'+str(scalefactor)+"*"+cuts[y])
 		BackgroundValues[y]+=(h.Integral())
-		#print str(y) +' of '+ str(numcut)
-	#print BackgroundValues
-	
+	del t
+	del T
+	del h
 num = '0123456789'
 
 
@@ -229,14 +277,17 @@ for x in range(len(SignalType)):
 		if q in num:
 			mass +=q
 	mass = float(mass)
-	print mass
-	if mass<460:
-		#print 'OK GOOD'
-		continue
 			
-	
 	BestSignif = -999.9
+	BestSignifSB = -999.9
+	BestSignifSSB = -999.9
+	BestSignifSSBdB2 = -999.9
+
 	BestCut = ''
+	BestCutSB = ''
+	BestCutSSB = ''
+	BestCutSSBdB2 = ''
+
 	sfile = FileLocation+'/'+SignalType[x]+'.root'
 	f = TFile.Open(sfile)
 	t = f.Get("PhysicalVariables")
@@ -251,18 +302,46 @@ for x in range(len(SignalType)):
 		T.Project('h','1.0',cut_mc+'*'+str(scalefactor)+"*"+cuts[y])
 		SignalValues[y]=(h.Integral())
 		SignifValues[y] = SignalValues[y] / math.sqrt(BackgroundValues[y] + SignalValues[y])
-		#print str(y) +' of ' +str(numcut)
-
+		
+		SignifValuesSSB[y] = SignalValues[y] / math.sqrt(BackgroundValues[y] + SignalValues[y])
+		SignifValuesSB[y] = SignalValues[y] / math.sqrt(BackgroundValues[y] )
+		SignifValuesSSBdB2[y] = SignalValues[y] / math.sqrt( BackgroundValues[y] + BGErrorFactor*BackgroundValues[y]*BackgroundValues[y] +  SignalValues[y])
+		
 		if SignifValues[y]>BestSignif:
 			BestSignif = SignifValues[y]
 			BestCut = cuts[y]
+
+		if SignifValuesSB[y]>BestSignifSB:
+			BestSignifSB = SignifValuesSB[y]
+			BestCutSB = cuts[y]
+			
+		if SignifValuesSSB[y]>BestSignifSSB:
+			BestSignifSSB = SignifValuesSSB[y]
+			BestCutSSB = cuts[y]
+						
+		if SignifValuesSSBdB2[y]>BestSignifSSBdB2:
+			BestSignifSSBdB2 = SignifValuesSSBdB2[y]
+			BestCutSSBdB2 = cuts[y]
+			
+	del T
+	del t
+	del h
 	print BestCut
 	print BestSignif
+	
 	logout.write(SignalType[x]+'\n')
 	logout.write(BestCut+'\n\n')
+	
+	logoutSB.write(SignalType[x]+'\n')
+	logoutSB.write(BestCutSB+'\n\n')
 
+	logoutSSB.write(SignalType[x]+'\n')
+	logoutSSB.write(BestCutSSB+'\n\n')	
 
+	logoutSSBdB2.write(SignalType[x]+'\n')
+	logoutSSBdB2.write(BestCutSSBdB2+'\n\n')	
 
-
-
-
+logoutSB.close()
+logoutSSB.close()
+logoutSSBdB2.close()
+logout.close()
