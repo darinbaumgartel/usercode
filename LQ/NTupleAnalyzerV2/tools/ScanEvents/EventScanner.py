350695060b61005b94b11167623a9cbcb6076565
darinb <>
2011-11-16 15:22:31 +0000

    updates

--- LQ/NTupleAnalyzerV2/tools/ScanEvents/EventScanner.py
+++ LQ/NTupleAnalyzerV2/tools/ScanEvents/EventScanner.py
@@ -16,7 +16,14 @@ del t
 def num2str(num, precision):
 	return "%0.*f" % (precision, num)
 
-
+mumuheader = '  \\begin{tabular}{|c||c|c|c|c||c|c|c|c||c|c||c|c||c|c|}\hline\n$S_T$&$p_T^{\mu}$&$\eta^{\mu}$&$\sigma_{p_T}^{\mu}$ & $q^{\mu}$&$p_T^{\mu}$&$\eta^{\mu}$&$\sigma_{p_T}^{\mu}$&$q^{\mu}$ &$p_T^{\\text{jet}}$&$\eta^{\\text{jet}}$&$p_T^{\\text{jet}}$&$\eta^{\\text{jet}}$&$\MET$\\\\ \hline \hline\n'
+munuheader = '  \\begin{tabular}{|c||c|c|c|c||c|c||c|c||c|c||c|c|}\hline\n$S_T$ &$p_T^{\mu}$ &$\eta^{\mu}$ &$\sigma_{p_T}^{\mu}$ & $q^{\mu}$ &$p_T^{\\text{jet}}$ &$\eta^{\\text{jet}}$ &$p_T^{\\text{jet}}$ &$\\eta^{\text{jet}}$ &$\MET$\\\\ \hline \hline'
+headers = []
+fillers = []
+xxx = []
+yyy = []
+zzz = []
+mutype = []
 print '\n----------  evaluating mumu   ----------\n\n'
 for n in range(tmumu.GetEntries()):
 	tmumu.GetEntry(n)
@@ -49,7 +56,17 @@ for n in range(tmumu.GetEntries()):
 	line += ' & '
 	line += num2str(tmumu.MET_pf,2)	
 	line += ' \\\\ '
+	XXX = str(tmumu.run_number)
+	ZZZ =  str(tmumu.ls_number) 
+	YYY = str(tmumu.event_number)
+	
 	print line
+	headers.append(mumuheader)
+	fillers.append(line+' \\hline')
+	xxx.append(XXX)
+	yyy.append(YYY)
+	zzz.append(ZZZ)
+	mutype.append('$\\mu\\mu jj$')
 
 
 print '\n----------  evaluating munu   ----------\n\n'
@@ -77,8 +94,17 @@ for n in range(tmunu.GetEntries()):
 	line += num2str(tmunu.MET_pf,2)	
 	line += ' \\\\  '
 	print line
+	headers.append(munuheader)
 	
-	
+	XXX = str(tmunu.run_number)
+	ZZZ =  str(tmunu.ls_number) 
+	YYY = str(tmunu.event_number)	
+	fillers.append(line+' \\hline')
+	xxx.append(XXX)
+	yyy.append(YYY)
+	zzz.append(ZZZ)
+	mutype.append('$\\mu\\nu jj$')
+		
 myevents = []
 print '\n----------  events for mumu   ----------\n\n'
 for n in range(tmumu.GetEntries()):
@@ -96,13 +122,28 @@ for n in range(tmunu.GetEntries()):
 	print line
 	
 print '\n\n'
+import os
 
 datasets = []
 
 datasets.append(['/SingleMu/Run2011A-May10ReReco-v1/RECO',160329,163869]) 
 datasets.append(['/SingleMu/Run2011A-PromptReco-v4/RECO',165071,168437])
-datasets.append(['/SingleMu/Run2011A-05Aug2011-v1/RECO',170053,172619])
+#datasets.append(['/SingleMu/Run2011A-05Aug2011-v1/RECO',170053,172619])
+datasets.append(['/SingleMu/Run2011A-PromptReco-v5/RECO',170053,172619])
 datasets.append(['/SingleMu/Run2011A-PromptReco-v6/RECO',172620,173688])
+
+CMSSWversions = []
+for x in datasets:
+	dset = x[0]
+	os.system('rm log.txt')
+	os.system('dbsql \'find release where dataset = '+dset+'\' > log.txt')
+	logfile = open('log.txt','r')
+	for line in logfile:
+		if 'CMSSW' in line:
+			CMSSWversions.append(line.replace('\n',''))
+			break
+	logfile.close()
+print CMSSWversions
 
 crabinfo = []
 for ev in myevents:
@@ -113,15 +154,50 @@ for ev in myevents:
 			dset = d[0]
 	crabinfo.append(ev + ',' + dset)
 	
-import os
 f = open('makegetevents.sh','w')
 f.write('#!/bin/sh\n\n')
+os.system('rm runlocal.csh')
+os.system("echo \#\!\/bin\/csh > runlocal.csh")
+for x in CMSSWversions:
+	os.system('echo "cmsrel '+x+' ">>runlocal.csh')
 for x in crabinfo:
 	cdir = x.split(',')[0]
 	cdir = 'dir'+cdir.replace(':','_')
 	f.write('rm -r '+cdir+'\nmkdir '+cdir+'\ncd '+cdir+'\n')
+	for d in range(len(datasets)):
+		if x.split(',')[1] in datasets[d][0]:
+			thisrel = CMSSWversions[d]
+	
 	f.write('edmPickEvents.py "'+x.split(',')[1]+'" '+x.split(',')[0]+' --crab\n')
+	os.system('echo "cd '+thisrel+'/src ">>runlocal.csh')
+	os.system('echo cmsenv >>runlocal.csh')
+	os.system('echo "cd -" >> runlocal.csh')
+	os.system('edmPickEvents.py "'+x.split(',')[1]+'" '+x.split(',')[0]+' --output Event_'+(cdir.replace(':','_')).replace('dir','')+' >>runlocal.csh')
 	f.write('crab -create -cfg pickevents_crab.config\ncrab -submit\ncd - \n\n')
+	
 	
 f.close()
 os.system('chmod 777 makegetevents.sh')
+os.system('chmod 777 runlocal.csh')
+os.system('rm texlines.txt')
+os.system('touch texlines.txt')
+import sys
+if '--maketex' in sys.argv:
+	
+	for n in range(len(headers)):
+		frag = open('fragment.txt','r')
+		os.system('rm thisfragment.txt')
+		fout = open('thisfragment.txt','w')
+		for line in frag:
+			line = line.replace('xxx',xxx[n])
+			line = line.replace('yyy',yyy[n])
+			line = line.replace('zzz',zzz[n])
+			line = line.replace('HEADERLINE',headers[n])
+			line = line.replace('FILLERLINE',fillers[n])
+			line = line.replace('MUTYPE',mutype[n])
+			fout.write(line)
+		fout.close()
+		os.system('cat thisfragment.txt >> texlines.txt')
+			
+			
+		
