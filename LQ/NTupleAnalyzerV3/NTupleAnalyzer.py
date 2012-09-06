e9f813a3f9e9fb713885d81b3ef8ada2714cf656
darinb <>
2012-09-06 21:38:35 +0000

    Functional branches and muon/jet filtering

--- LQ/NTupleAnalyzerV3/NTupleAnalyzer.py
+++ LQ/NTupleAnalyzerV3/NTupleAnalyzer.py
@@ -1,121 +1,83 @@
 import sys
-sys.argv.append( '-b' )
+sys.argv.append( '-b True' )
 from ROOT import *
-from array import array
+import array
 
-name = sys.argv[1]
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename", help="input root file", metavar="FILE")
+parser.add_option("-b", "--batch", dest="dobatch", help="run in batch mode", metavar="BATCH")
+parser.add_option("-s", "--sigma", dest="crosssection", help="specify the process cross-section", metavar="SIGMA")
+parser.add_option("-n", "--ntotal", dest="ntotal", help="total number of MC events for the sample", metavar="NTOTAL")
+(options, args) = parser.parse_args()
+
+name = options.filename
+if '/store' in name:
+	name = 'root://eoscms//eos/cms'+name
 
 fin = TFile.Open(name,"READ")
-tin = fin.Get("rootTupleTree/tree")
-hin = fin.Get("LJFilter/EventCount")
+t = fin.Get("rootTupleTree/tree")
 
-fout = TFile.Open((name.split('/')[-1]).replace('.root','skim.root'),"RECREATE")
+N = t.GetEntries()
+fout = TFile.Open(name.split('/')[-1].replace('.root','_tree.root'),"RECREATE")
+tout=TTree("PhysicalVariables","PhysicalVariables")
 
-savdir = gDirectory
-h1dir = savdir.mkdir("LJFilter")
-adir = savdir.mkdir("rootTupleTree")
-h2dir = h1dir.mkdir("EventCount")
-h2dir.cd()
-h = fin.Get("LJFilter/EventCount/EventCounter")
-h.Write()
-savdir.cd()
-adir.cd()
+branches = ['Et_miss','Pt_muon1','Pt_muon2','Pt_jet1','Pt_jet2','Pt_ele1','Pt_ele2']
 
-tout = tin.CopyTree("0")
+for b in branches:
+	print(b+' = array.array("f",[0])')
+	print('tout.Branch("'+b+'",'+b+',"'+b+'/F")' )
+	exec(b+' = array.array("f",[0])')
+	exec('tout.Branch("'+b+'",'+b+',"'+b+'/F")' )
 
-N = tin.GetEntries()
+def FilterJets(pt,eta,phi,looseid):
+	jets=[]
+	for n in range(len(pt)):
+		if pt[n]>20 and abs(eta[n])<2.4 and looseid[n]==1:
+			j = TLorentzVector()
+			j.SetPtEtaPhiM(pt[n],eta[n],phi[n],0)
+			jets.append(j)
+	return jets
+
+def FilterMuons(pt, eta, phi, isglobal, istracker, trkiso, stationmatches, vertexdxy, chi2, pixelhits, trackerhits, trkvalidhits):
+	muons = []
+	for n in range(len(pt)):
+		Pass = True
+		Pass *= (pt[n] > 40)
+		Pass *= abs(eta[n])<2.1
+		Pass *= isglobal[n]*istracker[n]
+		Pass *= (trkiso[n]/pt[n])<0.1
+		Pass *= trackerhits[n]>=11
+		Pass *= stationmatches[n]>1
+		Pass *= abs(vertexdxy[n])<0.2
+		Pass *= chi2[n]<10.0
+		Pass *= pixelhits[n]>=1
+		Pass *= trkvalidhits[n]>=1
+		if (Pass):
+			m = TLorentzVector()
+			m.SetPtEtaPhiM(pt[n],eta[n],phi[n],0)
+			muons.append(m)
+	return muons
+
 
 for n in range(N):
-	tin.GetEntry(n)
-	
-	if n%5000==0:
+	t.GetEntry(n)
+	if n%100==0:
 		print n, 'of', N
-		
-	ept1=0
-	mpt1=0
-	jpt1=0
-	jpt2=0
 
-	
-	ept2=0
-	mpt2=0
-	
-	met = 0
-	
-	jetpts = []
-	
-	if len(tin.ElectronPt)> 0:
-		ept1 = tin.ElectronPt[0]	
 
-	if len(tin.MuonPt)> 0:
-		mpt1 = tin.MuonPt[0]	
+	met = TLorentzVector()
+	met.SetPtEtaPhiM(t.PFMET[0],0,t.PFMETPhi[0],0)
 
-	for x in range(len(tin.PFJetPt)):
-		if (tin.PFJetPassLooseID[x] == 1) and ( abs(tin.PFJetEta[x]) < 2.5) :
-			jetpts.append(tin.PFJetPt[x])
-	
-	jetpts.append(0)
-	jetpts.append(0)
-	
-	jpt1 = jetpts[0]
-	jpt2=jetpts[1]
+	jets = FilterJets(t.PFJetPt,t.PFJetEta,t.PFJetPhi,t.PFJetPassLooseID)
+	muons =FilterMuons(t.MuonPt, t.MuonEta, t.MuonPhi, t.MuonIsGlobal, t.MuonIsTracker, t.MuonTrackerIsoSumPT, t.MuonStationMatches, t.MuonPrimaryVertexDXY,t.MuonGlobalChi2, t.MuonPixelHitCount, t.MuonTrkHitsTrackerOnly, t.MuonGlobalTrkValidHits)
 
-	if len(tin.ElectronPt)> 1:
-		ept2 = tin.ElectronPt[1]	
 
-	if len(tin.MuonPt)> 1:
-		mpt2 = tin.MuonPt[1]	
 
-	met = tin.PFMET[0]
 
-	leps = [met,ept1,ept2,mpt1,mpt2]
-	
-	leps.sort()
-	
-	l1 = leps[4]
-	l2 = leps[3]	
+	Et_miss[0] = met.Pt()
 
-	keep = 0
 
-	if ( ept1 > 35 and ept2 > 35 and jpt1>25):
-		keep = 1
-	
-	if ( ept1 > 35 and mpt1 > 35 and jpt1>25):
-		keep = 1	
-
-	if ( mpt1 > 35 and mpt2 > 35 and jpt1>25):
-		keep = 1	
-
-	if ( mpt1 > 35 and met > 45 and jpt1>25):
-		keep = 1
-
-	if ( ept1 > 35 and met > 45 and jpt1>25):
-		keep = 1
-		
-	if (keep ==0):
-		continue
-		
-	keep2 = 0
-		
-	if ( (ept1 + ept2 + jpt1 + jpt2) > 230 ):
-		keep2 = 1
-		
-	if ( (ept1 + met + jpt1 + jpt2) > 230 ):
-		keep2 = 1
-		
-	if ( (mpt1 + mpt2 + jpt1 + jpt2) > 230 ):
-		keep2 = 1
-		
-	if ( (mpt1 + met + jpt1 + jpt2) > 230 ):
-		keep2 = 1
-		
-	if ( (ept1 + mpt1 + jpt1 + jpt2) > 230 ):
-		keep2 = 1
-
-	if (keep2 == 0):
-		continue
-	
-		
 	tout.Fill()
 
 fout.Write()
