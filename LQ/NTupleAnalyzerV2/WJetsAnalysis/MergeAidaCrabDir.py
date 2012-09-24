a24cc6cccbc7219f4557539ecd5b61046de8ca91
darinb <>
2012-09-24 20:26:49 +0000

    updated for root files

--- LQ/NTupleAnalyzerV2/WJetsAnalysis/MergeAidaCrabDir.py
+++ LQ/NTupleAnalyzerV2/WJetsAnalysis/MergeAidaCrabDir.py
@@ -4,20 +4,52 @@ import os
 
 crabdir = sys.argv[1]
 
+rename =''
+if len(sys.agrv>1):
+	rename = sys.argv[2]
+
+
+
 aidas = [(crabdir + '/res/'+x.replace('\n','')).replace('//','/') for x in os.popen('ls -1 '+crabdir+'/res | grep ".aida"').readlines()]
 
+roots = [(crabdir + '/res/'+x.replace('\n','')).replace('//','/') for x in os.popen('ls -1 '+crabdir+'/res | grep ".root"').readlines()]
+
+
 aidaout = crabdir.replace('/','')+'.aida'
+
+rootout = crabdir.replace('/','')+'.root'
+
+if (rename!=''):
+	os.system('mv '+aidaout+ ' '+rename+'.aida')
+	os.system('mv '+rootout+ ' '+rename+'.root')
+
+	aidaout = rename+'.aida'
+	rootout = rename+'.root'
+	os.system()
+
 
 if aidaout in str(os.listdir('.')):
 	os.system('rm '+aidaout)
 	print 'Removing old aida file'
 
+if rootout in str(os.listdir('.')):
+	os.system('rm '+rootout)
+	print 'Removing old root file'
+
 aidamerge = './aidamerge -o '+aidaout
+
+rootmerge = 'hadd '+aidaout
+
 
 for x in aidas:
 	aidamerge += ' '+x
 
 
+for x in roots:
+	rootmerge += ' '+x
+
+
+os.system(rootmerge)
 
 f = open('tempmerge.sh','w')
 f.write('#!/bin/sh\n\ncd /afs/cern.ch/cms/slc5_amd64_gcc434/cms/cmssw/CMSSW_4_2_5/src\neval `scramv1 runtime -sh`\ncd -\n\nsource setupRivetProf.sh\n\n'+aidamerge+'\n\nrivet-mkhtml '+aidaout+'\n\n')
