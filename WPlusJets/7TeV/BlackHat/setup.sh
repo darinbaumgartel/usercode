341734780de7f0e52f168d6c4d654a341c1f287f
darinb <>
2013-07-12 15:55:52 +0000

    first commit

---
+++ WPlusJets/7TeV/BlackHat/setup.sh
@@ -0,0 +1,6 @@
+export SCRAM_ARCH=slc5_amd64_gcc462
+cmsenv
+export PATH=/afs/cern.ch/user/j/jhaley/work/CMSSW_5_3_8_LQ/src/ntuplesreader-1.0-install/bin:${PATH}
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/cern.ch/user/j/jhaley/work/CMSSW_5_3_8_LQ/src/ntuplesreader-1.0-install/lib:/afs/cern.ch/cms/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_3_8/external/slc5_amd64_gcc462/bin/../../../../../../lcg/root/5.32.00-cms21/lib:/afs/cern.ch/user/j/jhaley/work/CMSSW_5_3_8_LQ/src/fastjet-2.4.4-install/lib
+
