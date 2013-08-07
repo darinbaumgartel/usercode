b0462aba010b8a1e17ba3695fa31820e9d093ffd
darinb <>
2013-08-07 17:17:38 +0000

    directory name fixes

--- WPlusJets/7TeV/BlackHat/setup.sh
+++ WPlusJets/7TeV/BlackHat/setup.sh
@@ -1,6 +1,6 @@
 export SCRAM_ARCH=slc5_amd64_gcc462
 cmsenv
-export PATH=CURRENTDIR/ntuplesreader-1.0-install/bin:${PATH}
+export PATH=/afs/cern.ch/work/d/darinb/BHTEST1/CMSSW_5_3_8_Blackhat/src/BlackHat/ntuplesreader-1.0-install/bin:${PATH}
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:CURRENTDIR/ntuplesreader-1.0-install/lib:/afs/cern.ch/cms/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_3_8/external/slc5_amd64_gcc462/bin/../../../../../../lcg/root/5.32.00-cms21/lib:CURRENTDIR/fastjet-2.4.4-install/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/cern.ch/work/d/darinb/BHTEST1/CMSSW_5_3_8_Blackhat/src/BlackHat/ntuplesreader-1.0-install/lib:/afs/cern.ch/cms/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_3_8/external/slc5_amd64_gcc462/bin/../../../../../../lcg/root/5.32.00-cms21/lib:/afs/cern.ch/work/d/darinb/BHTEST1/CMSSW_5_3_8_Blackhat/src/BlackHat/fastjet-2.4.4-install/lib
 
