a831a4c75c0335c374d3270a88cf5805080aff77
darinb <>
2013-09-18 20:58:17 +0000

    jet update 2

--- RivetLQ/Rivet_LQ_TEST_FromCentralGen.py
+++ RivetLQ/Rivet_LQ_TEST_FromCentralGen.py
@@ -28,114 +28,12 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('file:SourceFile')
 )
 
-
-
-process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
-    splitLevel = cms.untracked.int32(0),
-    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
-    outputCommands = process.RAWSIMEventContent.outputCommands,
-    fileName = cms.untracked.string('gen.root'),
-    dataset = cms.untracked.PSet(
-        filterName = cms.untracked.string(''),
-        dataTier = cms.untracked.string('GEN')
-    ),
-    SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('generation_step')
-    )
-)
-
-
 process.GlobalTag.globaltag = 'MC_60_V4::All'
-process.generator = cms.EDFilter("Pythia6HadronizerFilter",
-    ExternalDecays = cms.PSet(
-        Tauola = cms.untracked.PSet(
-            UseTauolaPolarization = cms.bool(True),
-            InputCards = cms.PSet(
-                mdtau = cms.int32(0),
-                pjak2 = cms.int32(0),
-                pjak1 = cms.int32(0)
-            )
-        ),
-        parameterSets = cms.vstring('Tauola')
-    ),
-    UseExternalGenerators = cms.untracked.bool(True),
-    pythiaPylistVerbosity = cms.untracked.int32(1),
-    pythiaHepMCVerbosity = cms.untracked.bool(True),
-    comEnergy = cms.double(7000.0),
-    jetMatching = cms.untracked.PSet(
-        MEMAIN_showerkt = cms.double(0),
-        MEMAIN_maxjets = cms.int32(-1),
-        MEMAIN_minjets = cms.int32(-1),
-        MEMAIN_qcut = cms.double(-1),
-        MEMAIN_excres = cms.string(''),
-        MEMAIN_etaclmax = cms.double(-1),
-        MEMAIN_nqmatch = cms.int32(5),
-        outTree_flag = cms.int32(0),
-        scheme = cms.string('Madgraph'),
-        mode = cms.string('auto')
-    ),
-    maxEventsToPrint = cms.untracked.int32(0),
-    PythiaParameters = cms.PSet(
-        pythiaUESettings = cms.vstring('MSTU(21)=1     ! Check on possible errors during program execution', 
-            'MSTJ(22)=2     ! Decay those unstable particles', 
-            'PARJ(71)=10 .  ! for which ctau  10 mm', 
-            'MSTP(33)=0     ! no K factors in hard cross sections', 
-            'MSTP(2)=1      ! which order running alphaS', 
-            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
-            'MSTP(52)=2     ! work with LHAPDF', 
-            'PARP(82)=1.832 ! pt cutoff for multiparton interactions', 
-            'PARP(89)=1800. ! sqrts for which PARP82 is set', 
-            'PARP(90)=0.275 ! Multiple interactions: rescaling power', 
-            'MSTP(95)=6     ! CR (color reconnection parameters)', 
-            'PARP(77)=1.016 ! CR', 
-            'PARP(78)=0.538 ! CR', 
-            'PARP(80)=0.1   ! Prob. colored parton from BBR', 
-            'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter', 
-            'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter', 
-            'PARP(62)=1.025 ! ISR cutoff', 
-            'MSTP(91)=1     ! Gaussian primordial kT', 
-            'PARP(93)=10.0  ! primordial kT-max', 
-            'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default', 
-            'MSTP(82)=4     ! Defines the multi-parton model'),
-        processParameters = cms.vstring('MSEL=0         ! User defined processes', 
-            'PMAS(5,1)=4.8   ! b quark mass', 
-            'PMAS(6,1)=172.5 ! t quark mass', 
-            'MSTJ(1)=1       ! Fragmentation/hadronization on or off', 
-            'MSTP(61)=1      ! Parton showering on or off'),
-        parameterSets = cms.vstring('pythiaUESettings', 
-            'processParameters')
-    )
-)
 
 
-# Path and EndPath definitions
-process.generation_step = cms.Path(process.pgen)
-process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
-process.endjob_step = cms.EndPath(process.endOfProcess)
-#process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+process.load("GeneratorInterface.RivetInterface.rivetAnalyzer_cfi")
 
-# Schedule definition
-process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step)
-#process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step,process.RAWSIMoutput_step)
-# filter all path with the production filter sequence
-for path in process.paths:
-    getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+process.rivetAnalyzer.AnalysisNames = cms.vstring('CMS_LQ_TEST')
+process.rivetAnalyzer.UseExternalWeight = cms.bool(True)
 
-# customisation of the process
-
-
-# Automatic addition of the customisation function from Configuration.GenProduction.rivet_customize
-
-def customise(process):
-        process.load('GeneratorInterface.RivetInterface.rivetAnalyzer_cfi')
-        process.rivetAnalyzer.AnalysisNames = cms.vstring('MC_LES_HOUCHES_SYSTEMATICS_CMS','MC_LES_HOUCHES_SYSTEMATICS_ATLAS','MC_LES_HOUCHES_SYSTEMATICS_CDF')
-        process.generation_step+=process.rivetAnalyzer
-#        process.schedule.remove(process.RAWSIMoutput_step)
-        return(process)
-
-
-
-process = customise(process)
-
-
-# End of customisation functions
+process.p = cms.Path(process.rivetAnalyzer)
